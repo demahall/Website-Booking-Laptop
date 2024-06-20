3dcecf8004a0729475404821371477aac79e25b4
demahall <104644825+demahall@users.noreply.github.com>
2024-06-20 11:06:08 +0200

    main ready to launch automatically after restarting computer

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from website import create_app,run_migrations
+from waitress import serve
 
 app = create_app()
 
-
+myIP= '192.168.178.134'
 if __name__ == '__main__':
     #run_migrations("Add Comment Section into Booking Table")
-    app.run(debug=True)
+    #app.run(debug=True)
+    serve(app, host=myIP, port=5000)
 
 
